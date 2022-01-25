1358cc39513a1e8c1780ec4e2a8cb5e52b820f91
Jphn <joaopedroholandaneves@gmail.com>
2022-01-25 18:44:41 -0300

    again

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css'
 
-// import { HashRouter, Route, Routes } from 'react-router-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages'
 import SigninPage from './pages/signin'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="signin" element={<SigninPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
